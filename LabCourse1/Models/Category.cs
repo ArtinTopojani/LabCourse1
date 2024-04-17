03c323f2ef329fad7044d21d8881d63433a43629
Artin Topojani <artintopojani2002@gmail.com>
2024-04-17 14:58:35 +0200

    Category CRUD completed

--- LabCourse1/Models/Category.cs
+++ LabCourse1/Models/Category.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LabCourse1.Models
 {
     public class Category
     {
-        
+
         public int Id { get; set; }
         [Required]
+        [MaxLength(20)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be in range of 1-100") ]
         public int DisplayOrder { get; set; }
     }
 }
